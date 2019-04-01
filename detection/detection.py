b68ca3f276f7cd9738ee950dc70f59ca3445e86b
Fortunato Trunfio <f.trunfio@studenti.unisa.it>
2019-04-01 20:31:59 +0200

    Modifica detection

--- detection/detection.py
+++ detection/detection.py
@@ -5,48 +5,45 @@ import sys
 def read_prot(file_name):
     res = list()
     names = list()
-    classes = {i: list() for i in range(19)}
+    classes = dict()
     j = 0
+    sift = cv2.xfeatures2d.SIFT_create()
     with open(file_name) as csvfile:
         spam_reader = csv.reader(csvfile, delimiter=',')
         for row in spam_reader:
             names.append(row[0])
             index = int(float(row[1]))
-            classes[index].append(j)
+            classes[j]=index
             j += 1
-            img = cv2.imread('./prot/' + row[0], 0)
-            th1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                        cv2.THRESH_BINARY, 11, 2)
-            res.append(th1)
+            img = cv2.imread('./prot/' + row[0],0)
+            kp1, des1 = sift.detectAndCompute(img, None)
+            res.append(des1)
     return res, names, classes
 
 def read_test(file_name):
     res = list()
     names = list()
     classes = dict()
+    sift = cv2.xfeatures2d.SIFT_create()
     with open(file_name) as csvfile:
         spam_reader = csv.reader(csvfile, delimiter=',')
         for row in spam_reader:
             names.append(row[0])
-            tmp = cv2.imread('./test/' + row[0], 0)
-            th1 = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                        cv2.THRESH_BINARY, 11, 2)
+            img = cv2.imread('./test/' + row[0],0)
             classes[row[0]] = int(float(row[1]))
-            img = th1[int(float(row[4])):int(float(row[5])), int(float(row[2])):int(float(row[3]))]
-            res.append(img)
+            kp1, des1 = sift.detectAndCompute(img, None)
+            res.append(des1)
     return res, names, classes
 
-def classification(img1,img2,sift,bf):
+def classification(img1,img2,bf):
     try:
-        kp1, des1 = sift.detectAndCompute(img1, None)
-        kp2, des2 = sift.detectAndCompute(img2, None)
         points=0
-        if des1 is not None and des2 is not None:
-            matches = bf.match(des1, des2)
+        if img1 is not None and img2 is not None:
+            matches = bf.match(img1, img2)
             matches = sorted(matches, key=lambda x: x.distance)
             for m in matches:
-                if m.distance < 224:
-                    points += (224-m.distance)
+                if m.distance < 110:
+                    points += (110-m.distance)
         return points
     except Exception as ex:
         print(ex)
@@ -55,38 +52,29 @@ def write(result, names_test,name_file):
     with open(name_file, mode='w') as csvfile:
         spam_writer=csv.writer(csvfile,delimiter=',')
         for i in names_test:
-            spam_writer.writerow([i,result[i]])
+            spam_writer.writerow([result[i]])
 
 
 if __name__=='__main__':
     list_prot,names_prot,classes_prot=read_prot('prot.csv')
+    print("prot letto")
     list_test,names_test,classes_test=read_test(sys.argv[1])
+    print("test letto")
     result={i:0 for i in names_test}
-    sift = cv2.xfeatures2d.SIFT_create()
     bf = cv2.BFMatcher()
     for i in range(len(list_test)):
         res = list()
-        for j in range(len(classes_prot)):
-            if len(classes_prot[j]) !=0:
-                index=classes_prot[j][0]
-                val=classification(list_test[i],list_prot[index],sift,bf)
-                res.append(j)
-        if len(res)==1:
-            index=names_test[i]
-            result[index]=res[0]
-        elif len(res)==0:
-            index = names_test[i]
-            result[index]=-1
-        else:
-            val_max=0
-            val_index=0
-            for j in res:
-                for ind in classes_prot[j]:
-                    val = classification(list_test[i], list_prot[ind], sift, bf)
-                    if val>val_max:
-                        val_index=j
-            index = names_test[i]
-            result[index]=val_index
+        print(names_test[i])
+        val_max=0
+        val_index=0
+        for j in range(len(names_prot)):
+            val = classification(list_test[i], list_prot[j], bf)
+            if val>val_max:
+                val=val_max
+                val_index=j
+        print("Val index: " + str(classes_prot[val_index]))
+        index = names_test[i]
+        result[index] = classes_prot[val_index]
     tot=0
     write(result,names_test,sys.argv[2])
     for i in names_test:
@@ -94,6 +82,6 @@ if __name__=='__main__':
         val2=classes_test[i]
         if val1==val2:
             tot+=1
-    acc=(tot/len(names_test))*100
+    acc=tot/len(names_test)*100
     print('Accuracy: '+str(acc))
 
